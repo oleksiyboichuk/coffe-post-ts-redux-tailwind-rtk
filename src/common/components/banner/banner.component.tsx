d17070ef560d5f7a1d6bf79e9fde3e8b826ee60d
_boy4uk <116024276+oleksiyboichuk@users.noreply.github.com>
2023-11-30 00:27:21 +0200

    add

--- src/common/components/banner/banner.component.tsx
+++ src/common/components/banner/banner.component.tsx
@@ -35,7 +35,7 @@ export const Banner: FC<BannerProps> = () => {
         <div className="bg-conduit-300 shadow-banner text-white p-8 mb-8">
             <Container>
                 <motion.h1 className="text-4xl font-titillium drop-shadow-logo text-center lg:text-logo pb-2 leading-tight" initial={pVariants.hidden} animate={pVariants.visible} transition={{ duration: 1 }} variants={pVariants}>We are not an analogue of <span className='text-conduit-200'>Reddit</span><div className='text-yellow-200'>We are different!</div></motion.h1>
-                <motion.p className="text-xl text-center lg:text-2xl font-light text-white" initial={pVariants.hidden2} animate={pVariants.visible} transition={{ duration: 1 }} variants={pVariants}>Are you thinking about something? -
+                <motion.p className="text-xl text-center lg:text-2xl font-light text-white" initial={pVariants.hidden2} animate={pVariants.visible} transition={{ duration: 1 }} variants={pVariants}>Are you thinking about something?<div className='flex lg:hidden'></div>
                     <span className='font-bold text-conduit-200'> {text}</span>
                     <Cursor />
                 </motion.p>
--- src/common/components/header/burger-menu.component.tsx
+++ src/common/components/header/burger-menu.component.tsx
@@ -13,6 +13,7 @@ const BurgerMenu: React.FC<BurgerMenuProps> = ({ isOpen, onClose }) => {
     return (
         <div className={clsx('fixed inset-0 bg-black/70 z-50 transition-transform ease-out duration-200', { 'translate-x-full': !isOpen })} onClick={onClose}>
             <div className="fixed top-0 right-0 bottom-0 flex items-center justify-center bg-white w-[60%] transition-transform ease-out">
+                <div className='absolute right-0 top-0 pr-4 pt-2 text-3xl font-bold text-conduit-500 drop-shadow-md' onClick={onClose}>X</div>
                 <ul className="text-white text-2xl">
                     <li className="mb-4">
                         <NavLink to="/" onClick={onClose} className="text-conduit-200 text-3xl hover:text-conduit-300 transition-color duration-200">
