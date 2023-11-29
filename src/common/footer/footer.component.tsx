69a29afa1db121e4e6873321e044a82efc6c1868
_boy4uk <116024276+oleksiyboichuk@users.noreply.github.com>
2023-11-29 21:19:04 +0200

    addd

--- src/common/footer/footer.component.tsx
+++ src/common/footer/footer.component.tsx
@@ -7,7 +7,7 @@ export const Footer: FC<FooterProps> = () => {
         <footer className='fixed bottom-0 w-full bg-conduit-200 py-2 lg:py-3'>
             <div className='flex justify-center items-center space-x-2'>
                 <i className='ion-social-github text-white text-lg lg:text-2xl'></i>
-                <a href="https://github.com/oleksiyboichuk/react-ts-redux-rtk-tailwind" className='text-white hover:text-white hover:underline text-lg lg:text-xl' target='_blunk'>Project on GitHub</a>
+                <a href="https://github.com/oleksiyboichuk/coffe-post-ts-redux-tailwind-rtk" className='text-white hover:text-white hover:underline text-lg lg:text-xl' target='_blunk'>Project on GitHub</a>
             </div>
         </footer>
     )
